c467da848d65f99eb44f79340f23cb618b6b2984
Saturn5forever <lukec9932@gmail.com>
2026-03-23 20:30:25 -0600

    Boosting added
    I added boosting and a reference object.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,19 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float maxSpeed = 10f;
+    public float moveSpeed;
     public InputAction MoveKeys;
     public InputAction RotateKeys;
+    public InputAction BoostKey;
     Rigidbody2D rb;
+
+    public float boostSpeed = 25f;
+    public bool canBoost;
+    public float boostTime = 3f;
+    public float boostCooldown = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveKeys.Enable();
         RotateKeys.Enable();
+        BoostKey.Enable();
         rb = GetComponent<Rigidbody2D>();
+        canBoost = true;
+        moveSpeed = maxSpeed;
     }
 
     // Update is called once per frame
@@ -23,5 +34,23 @@ public class Movement : MonoBehaviour
         rb.AddRelativeForce(moveDirection * moveSpeed, ForceMode2D.Force);
         Vector2 rotationDirection = RotateKeys.ReadValue<Vector2>();
         transform.Rotate(transform.rotation.x, transform.rotation.y, rotationDirection.x);
+
+        if (BoostKey.WasPressedThisFrame() && canBoost == true)
+        {
+            moveSpeed = boostSpeed;
+            canBoost = false;
+            Invoke("ResetBoosting", boostTime);
+
+        }
+    }
+
+    void ResetBoosting()
+    {
+        moveSpeed = maxSpeed;
+        Invoke("BoostingCooldown", boostCooldown);
+    }
+    void BoostingCooldown()
+    {
+        canBoost = true;
     }
 }
