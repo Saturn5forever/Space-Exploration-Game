ac417bf9f9ffec4e25ff1aa81f7c3a7061b08c23
Saturn5forever <lukec9932@gmail.com>
2026-03-26 14:57:28 -0600

    Added Timer and Highscores
    I added a timer and a highscore system that saves over sessions. I also added a magnet ability but I've decided to change the type of game to a racing space game. This is because pushing the box around felt to difficult and it would not have worked out in my timeframe. The logic for the magnet is still there as of now incase I revert it again.

--- Assets/Scripts/Crate Finish.cs
+++ Assets/Scripts/Crate Finish.cs
@@ -1,18 +1,71 @@
-using Unity.Cinemachine;
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrateFinish : MonoBehaviour
 {
-    public GameObject attractorBeam;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI highScoreText;
+    private float roundedTime;
+    public float finishTime;
+    private string highScoreKey;
+
+    private bool timerActive = true;
 
     void Awake()
     {
+        timerText.enabled = true;
+    }
+
+    void Start()
+    {
+        highScoreKey = "Highscore_" + SceneManager.GetActiveScene().name;
+        LoadHighScore();
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Finish"))
         {
             collision.gameObject.GetComponentInChildren<Gravity>().enabled = true;
+            finishTime = roundedTime;
+            timerActive = false;
+            ChangeHighScore();
         }
     }
-}
+
+    void Update()
+    {
+        if (timerActive)
+        {
+            roundedTime = Mathf.Round(Time.time * 100f) / 100f;
+            timerText.text = roundedTime.ToString();     
+        }
+            
+    }
+    
+    void LoadHighScore()
+    {
+        float savedHighScore = PlayerPrefs.GetFloat(highScoreKey, 0);
+        highScoreText.text = "High: " + savedHighScore.ToString();
+    }
+
+    void ChangeHighScore()
+    {
+        float savedHighScore = PlayerPrefs.GetFloat(highScoreKey, 0);
+        if(finishTime < savedHighScore)
+        {
+            PlayerPrefs.SetFloat(highScoreKey, finishTime);
+            PlayerPrefs.Save();
+            Debug.Log("SAVED HIGHSCORE");
+            UpdateUI();
+        }
+    }
+
+    void UpdateUI()
+    {
+        float savedHighScore = PlayerPrefs.GetFloat(highScoreKey, 0);
+        highScoreText.text = "High: " + savedHighScore; 
+    }
+}   
+
